e5cd6d72f2e424add40758bc9032552cdc474a0c
Chae-G-0 <corkdud@naver.com>
2026-04-26 21:06:50 +0900

    style: home 스타일 변경

--- src/components/page/IndexPage.tsx
+++ src/components/page/IndexPage.tsx
@@ -4,13 +4,17 @@ import RegionCard from "../home/RegionCard";
 export default function Home() {
   const { isPc } = useResponsive();
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center p-4">
-      <p className="mb-[-6px] text-[16px]">
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-[#F5F9FC] p-4">
+      <p
+        className={`${isPc ? `text-[16px]` : `text-[14px]`} mb-[-6px] text-gray-800`}
+      >
         크로스핏도 여행의 일부인 핏터들을 위한
       </p>
-      <p className={`${isPc ? `text-[36px]` : `text-[26px]`} mb-2 font-bold`}>
-        전국 824개 크로스핏 BOX MAP
-      </p>
+      <div
+        className={`${isPc ? `text-[36px]` : `text-[20px]`} mb-2 font-bold text-gray-800`}
+      >
+        전국 826개 크로스핏 BOX MAP
+      </div>
       <RegionCard />
     </div>
   );
